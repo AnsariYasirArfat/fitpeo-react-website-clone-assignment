cdfb38b55c389c8fa0cf2f41bbf7295442045410
AnsariYasirArfat <ansariyaseer786@gmail.com>
2025-06-24 09:12:08 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <div className='flex flex-col items-center justify-center h-screen'>
+    <h1 className='text-4xl font-bold text-red-400'>FitPeo React Website clone assignment</h1>
+   </div>
+  )
+}
+
+export default App
